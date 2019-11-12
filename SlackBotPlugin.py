805ae2bf9a0e703ba448750a1ba9afeee704e1fb
futuremaze <future.maze@gmail.com>
2019-11-12 22:29:45 +0900

    announce_if_in_time の仮実装

--- SlackBotPlugin.py
+++ SlackBotPlugin.py
@@ -5,10 +5,19 @@ import re
 import subprocess
 
 
+# グローバル定数
 EXEC_ANNOUNCE = "/usr/share/pi-jtalk/bin/announce.sh"
 
 # !announce 指定されたメッセージを発話する
-@respond_to('^.*!announce (.*)')
+@respond_to('^!announce (.*)')
 def announce(message, something):
     subprocess.call([EXEC_ANNOUNCE, something])
 
+# !announce_if_in_time hh-hh hh-hh時間内なら指定メッセージを発話する
+@respond_to('^!announce_if_in_time ([0-2][0-9]-[0-2][0-9]) (.*)')
+def announce_if_in_time(message, time, something):
+    # TODO: 時刻の解析
+    in_time = True
+
+    if (in_time):
+        announce(message, something)
