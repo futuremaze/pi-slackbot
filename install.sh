2d90be7c65bb65f27fb264a934c4b9e65b1a5e59
futuremaze <future.maze@gmail.com>
2019-11-13 22:38:20 +0900

    インストールスクリプトに dispatcher.py のパッチ適用

--- install.sh
+++ install.sh
@@ -29,6 +29,10 @@ function main() {
   sudo pip3 install --upgrade pip3
   sudo pip3 install slackbot
 
+  # パッチ適用
+  SLACKBOT_DIR="$(pip3 show slackbot | grep Location: | sed -e 's/^Location: //g')/slackbot"
+  patch -u ${SLACKBOT_DIR}/dispatcher.py < ./dispatcher.py.patch
+
   # スクリプトのインストール
   sudo mkdir -p ${INSTALL_DIR}
   sudo cp -R ./bin ${INSTALL_DIR}/
