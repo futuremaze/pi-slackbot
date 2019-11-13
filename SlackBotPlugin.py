513f316618602c2f1065d107193433032cad620e
futuremaze <future.maze@gmail.com>
2019-11-13 12:25:55 +0900

    announce_if_in_time の時間確認処理を実装

--- SlackBotPlugin.py
+++ SlackBotPlugin.py
@@ -3,7 +3,7 @@
 from slackbot.bot import respond_to, listen_to
 import re
 import subprocess
-
+import datetime
 
 # グローバル定数
 EXEC_ANNOUNCE = "/usr/share/pi-jtalk/bin/announce.sh"
@@ -16,8 +16,9 @@ def announce(message, something):
 # !announce_if_in_time hh-hh hh-hh時間内なら指定メッセージを発話する
 @respond_to('^!announce_if_in_time ([0-2]{0,1}[0-9]-[0-2][0-9]) (.*)')
 def announce_if_in_time(message, time, something):
-    # TODO: 時刻の解析
-    in_time = True
+    now = datetime.datetime.now()
+    time_s = time.split('-')
+    start, end = int(time_s[0]), int(time_s[1])
 
-    if (in_time):
+    if (start <= now.hour and end >= now.hour):
         announce(message, something)
